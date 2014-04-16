e613a9b93f2d493d8d866ed1c4514fc498f7932a
Roman Zubenko <romanz@bu.edu>
2014-04-16 19:21:19 -0400

    step three is done

--- parser.py
+++ parser.py
@@ -4,10 +4,42 @@ def parser(filename,text):
 	# step one
 	printOutput(text,"all",filename)
 	
-	text = re.sub(r'(I|We|You|They|a|and|the|that|of|for|with|)',"",text)
+	properText = re.sub(r'(I|We|You|They|a|and|the|that|of|for|with|)',"",text)
 
 	# step two
-	printOutput(text,"proper","")
+	printOutput(properText,"proper","")
+
+	'''
+		For step three I need to made some assumptions:
+			1. Articles are separated with "Article [0-9]*"
+			2. Sections are nested withing article and delimited with: "Section [0-9]*"
+	'''
+
+	articleCount = 0
+	sectionCounts = {}
+
+	articles = re.split(r"(\nArticle [0-9]*\.)",text)
+	articles = articles[1:]
+	for e in articles:
+		sectionCount = 0
+		if len(re.findall(r"Article [0-9]*\.",e)) == 0:
+			articleCount += 1
+			sections = re.split(r"(\nSection [0-9]*)",e)
+			for s in sections:
+				if len(re.findall(r"\nSection [0-9]*",s)) == 0:
+					if articleCount in sectionCounts:
+						sectionCounts[articleCount] += 1
+					else:
+						sectionCounts[articleCount] = 1
+		else:
+			pass
+
+	print("Articles: " + str(articleCount))
+	print("Total Sections per Article: ")
+	for i in range(1,8):
+		print("Article " + str(i) + ": "+ str(sectionCounts[i]))
+			
+
 	
 
 def printOutput(text,marker,name):
