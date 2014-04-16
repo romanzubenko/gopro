2e699c300442021ae5f1dede229adfd700bde9ed
Roman Zubenko <romanz@bu.edu>
2014-04-16 17:51:02 -0400

    call metrics correct except for word count

--- parser.py
+++ parser.py
@@ -1,4 +1,10 @@
-import sys
+import sys,re
+
+def parser(filename,text):
+	newLines = len(text.split("\n"))
+	words = len([string for string in re.split(r" |\t|\-|\.|\!\?|\(|\)",text) if string.isalpha()])
+	bytes = len(text) 
+	print("     " +str(newLines) + "    " + str(words) + "   " + str(bytes) + " " + filename)
 
 
 # if filename is not passed
@@ -7,19 +13,15 @@ if len(sys.argv) < 2:
 
 # get the first command line argument
 filename = sys.argv[1]
+
 # open file
 try:
 	fObject = open(filename, "r")
 	fileData = fObject.read()
-	parser(fileData)
-except:
-	print "parser: " + filename + ": open: No such file or directory"
+	
+except :
+	print("parser: " + filename + ": open: No such file or directory")
 	exit 
-# 
 
 
-def parser(text):
-	newLines = len(text.split("\n"))
-	words = len(text.split(" |\n"))
-	bytes = len(text.split("")) * 8
-	print newLines + " " + words + " " + bytes
+parser(filename,fileData)
